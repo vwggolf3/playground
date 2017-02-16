06d0cd66b5ba5a820ece9a4cead7a63fb1347f30
Gabriel Pop <vwggolf3@gmail.com>
2017-02-16 14:27:02 +0200

    test git integration from intelliJ

---
+++ src/test/java/TestArrayOperations.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by Gabi on 16/02/17.
+ */
+public class TestArrayOperations {
+
+    @Test
+    public void testSomething() {
+        System.out.println("gabi");
+    }
+
+}
